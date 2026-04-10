ae52ccff64db019c71df02ff7178312f96be7350
Sergio <sergio.montes84@uabc.edu.mx>
2026-04-10 13:52:19 -0700

    feat: Archivo del programa
    -Se creo el archivo del programa
    -Estructura para el codigo

---
+++ Examen-POO-U2y3.py
@@ -0,0 +1,25 @@
+#======== Clases Abstractas ========
+"""
+Agregar clases que se usan como bases para otras
+"""
+
+#======== Especializados ========
+"""
+Clases de los pokemones con tipo
+"""
+#======== Pokemon ========
+"""
+"""
+
+#======== Combate ========
+"""
+Logica de combate, creacion de enemigos
+"""
+#======== Utilidades ========
+"""
+Funciones extras para menus, ciclos fijos, etc.
+"""
+#======== Main ========
+"""
+Ejecucion del progama
+"""
